0f2671fceb724e2a26a9a47937b9e94696f8589a
Marcelina <trocmarcelina1@gmail.com>
2025-05-13 18:13:29 +0200

    Przekopiowanie kodu z vm do lab10.1.sh

--- lab10/lab10.sh
+++ lab10/lab10.sh
@@ -1,3 +1,24 @@
 #Napisz skrypt, który automatycznie loguje się na zdalny serwer przy
 #użyciu SSH, wykonuje kilka poleceń (np. listowanie plików, procesów) i
 #zapisuje wynik do pliku lokalnego.
+                                     
+#!/bin/bash
+
+# zmienne
+USER="kali"         # nazwa uzytkownika
+IP="10.0.2.15"  # adres serwera
+
+# Plik wynikowy
+OUTPUT="output.txt"
+
+# Wykonanie poleceń przez SSH i zapis wyniku do pliku
+ssh "$USER@$IP" '
+echo "Lista plikow ---------"
+ls ~
+echo "Procesy uzytkownika -------"
+ps -u $USER
+' > "$OUTPUT"
+# Komunikat o zakończeniu
+echo "Wyniki zostały zapisane w pliku: $OUTPUT"
+
+
